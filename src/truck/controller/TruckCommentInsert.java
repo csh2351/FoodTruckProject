71793a46276f922497069be5f79529b89ff99059
jazzhong1 <36650330+jazzhong1@users.noreply.github.com>
2018-06-12 20:17:49 +0900

    result

--- src/truck/controller/TruckCommentInsert.java
+++ src/truck/controller/TruckCommentInsert.java
@@ -14,6 +14,7 @@ import com.oreilly.servlet.MultipartRequest;
 
 import common.MyFileRenamePolicy;
 import truck.service.TruckService;
+import truck.vo.Truck;
 import truck.vo.TruckReviewComment;
 
 /**
@@ -22,84 +23,97 @@ import truck.vo.TruckReviewComment;
 @WebServlet("/truckCommentInsert")
 public class TruckCommentInsert extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TruckCommentInsert() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TruckCommentInsert() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		
-		if(!ServletFileUpload.isMultipartContent(request)){
+
+		if (!ServletFileUpload.isMultipartContent(request)) {
 			request.setAttribute("msg", "사진을불러올수없습니다[관리자에 문의하세요]");
 			request.setAttribute("loc", "/");
 			request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
 		}
-		
-		String root=getServletContext().getRealPath("/");
-		String saveDir=root+"/images"+File.separator+"truckReview";
-		System.out.println(saveDir);
-		
-		//크긱조정
-		int maxSize=1024*1024*10;
-		
-		MultipartRequest mpr=new MultipartRequest(request, saveDir, maxSize,"UTF-8", new MyFileRenamePolicy());
-		TruckReviewComment  comment=new TruckReviewComment();
-		int truckPk=Integer.parseInt(mpr.getParameter("truckPk"));
 
-		//db컬럼순으로 입력했음//
+		String root = getServletContext().getRealPath("/");
+		String saveDir = root + "/images" + File.separator + "truckReview";
+		System.out.println(saveDir);
+
+		// 크긱조정
+		int maxSize = 1024 * 1024 * 10;
+
+		MultipartRequest mpr = new MultipartRequest(request, saveDir, maxSize, "UTF-8", new MyFileRenamePolicy());
+		TruckReviewComment comment = new TruckReviewComment();
+		int truckPk = Integer.parseInt(mpr.getParameter("truckPk"));
+		Truck truck = new TruckService().selectOne(truckPk);
+
+		// db컬럼순으로 입력했음//
 		comment.setReviewCommentLevel(Integer.parseInt(mpr.getParameter("reviewCommentLevel")));
 		comment.setReviewCommnetWriter(mpr.getParameter("reviewCommentWriter"));
 		comment.setReviewCommnetContent(mpr.getParameter("reviewCommentContent"));
 		comment.setReviewCommentRef(Integer.parseInt(mpr.getParameter("reviewCommentRef")));
 		comment.setReviewCommentOimage(mpr.getOriginalFileName("up_file"));
 		comment.setReviewCommentRimage(mpr.getFilesystemName("up_file"));
-		comment.setReviewStar(Double.parseDouble(mpr.getParameter("reviewStar")));
+		double star;
+		if(mpr.getParameter("reviewStar")==null){
+			star=0;
+		}else {
+			star=Double.parseDouble(mpr.getParameter("reviewStar"));
+		}
+		comment.setReviewStar(star);
 		comment.setMemberPk(Integer.parseInt(mpr.getParameter("memberPk")));
 		comment.setTruckPk(truckPk);
-		
-		int result=new TruckService().insertTruckComment(comment);
-		System.out.println("결과 :"+result);
-		
-	/*	String checkFile=mpr.getOriginalFileName("up_file");
-		System.out.println(checkFile);
-		if(checkFile!=null) {
-		if(checkFile.contains(".jsp")||checkFile.contains(".exe")||checkFile.contains(".html")){
-			//삭제로직
-			File deleteFile=new File(saveDir+"/"+checkFile);
-			deleteFile.delete();
-			request.setAttribute("msg", "올릴수 없는파일입니다.[확장자명 확인]");
-			request.setAttribute("msg", "사진은 크기를 확인하세요");
-			System.out.println("결과"+result);
-		
+
+		int result = new TruckService().insertTruckComment(comment);
+		System.out.println("결과 :" + result);
+
+		String view = "";
+		String checkFile = mpr.getOriginalFileName("up_file");
+		if (checkFile != null) {
+			if (checkFile.contains(".jsp") || checkFile.contains(".exe") || checkFile.contains(".html")) {
+
+				// 삭제로직
+				File deleteFile = new File(saveDir + "/" + checkFile);
+				deleteFile.delete();
+
+				request.setAttribute("msg", "올릴수 없는파일입니다.[확장자명 확인]");
+
+				System.out.println("결과" + result);
+
 			}
-		}*/
-		
-		String view="";
-		if(result>0){
-			view="/views/truck/truckChoice.jsp";
+		}
+		if (result > 0) {
+			view = "/views/truck/truckChoice.jsp";
+			request.setAttribute("truck", truck);
 			request.setAttribute("truckChoice", "truckChoiceReview");
+		} else {
+			view = "/views/common/msg.jsp";
+			request.setAttribute("msg", "사진은 크기를 확인하세요");
+			request.setAttribute("loc", "/");
 		}
-		else {
-			view="/views/common/msg.jsp";
-			request.setAttribute("loc", view);
-		}
-		
+
 		request.getRequestDispatcher(view).forward(request, response);
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
